6cb99b0c7e53858cda9352791283af499d21ab17
EL ARFAOUI ZAKARIA <76808317+zakaria2105@users.noreply.github.com>
2022-05-16 11:27:50 +0100

    feat: :sparkles: add api route and error handler middleware

--- server/app.js
+++ server/app.js
@@ -1,10 +1,17 @@
 import express from "express";
 import morgan from "morgan";
+import errorHandler from "./middleware/errorHandler.js";
+import routes from "./routes/index.routes.js";
 
 const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use("/api/v1", routes);
+app.use("*", (_req, res) => {
+  res.status(404).json({ message: "Page not found" });
+});
+app.use(errorHandler);
 
 export default app;
