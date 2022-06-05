c1c83d8fceaa26cf2b372ff96be18985a1825fef
EL ARFAOUI ZAKARIA <76808317+zakaria2105@users.noreply.github.com>
2022-06-05 21:41:48 +0100

    feat: :sparkles: add get all elections function

--- server/controllers/election.controller.js
+++ server/controllers/election.controller.js
@@ -23,4 +23,44 @@ const addElection = asyncWrapper(async (req, res, next) => {
   });
 });
 
-export { addElection };
+const getAllElections = asyncWrapper(async (req, res, next) => {
+  const page = req.query.page ? parseInt(req.query.page, 10) : 0;
+  const limit = req.query.limit ? parseInt(req.query.limit, 10) : 10;
+  const { status } = req.query;
+
+  const pipeline = [
+    {
+      $project: {
+        title: 1,
+        startDate: 1,
+        endDate: 1,
+        status: 1,
+      },
+    },
+    {
+      $skip: page * limit,
+    },
+    {
+      $limit: limit,
+    },
+  ];
+
+  const match = {};
+
+  status && (match.status = status);
+  if (Object.keys(match).length > 0) pipeline.unshift({ $match: match });
+
+  const elections = await Election.aggregate(pipeline);
+  const totalElection = await Election.countDocuments();
+  const numberOfPages = Math.ceil(totalElection / limit);
+
+  res.status(200).json({
+    error: false,
+    totalElection: totalElection,
+    found: elections.length,
+    numberOfPages,
+    data: elections,
+  });
+});
+
+export { addElection, getAllElections };
