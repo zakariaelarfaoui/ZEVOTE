4f0b90b42a02b845ec0387f4f33d09623c00ff1f
EL ARFAOUI ZAKARIA <76808317+zakaria2105@users.noreply.github.com>
2022-06-10 10:46:42 +0100

    feat: :sparkles: add login route

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -4,4 +4,5 @@ const router = express.Router();
 import authController from "../controllers/auth.controller.js";
 
 router.post("/register", authController.register);
+router.post("/login", authController.login);
 export default router;
