b0fec176c06561f2f7327f7db8b1259cda7eb9a3
EL ARFAOUI ZAKARIA <76808317+zakaria2105@users.noreply.github.com>
2022-06-10 10:38:12 +0100

    feat: :sparkles: add login

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,11 +1,15 @@
 import jwt from "jsonwebtoken";
 import CheckDuplicateField from "../helpers/checkDuplicateField.js";
 import newError from "../helpers/customError.js";
-import hashField from "../helpers/hashField.js";
+import hashField, { compareHash } from "../helpers/hashField.js";
 import mailer from "../helpers/mailer.js";
+import { generateToken } from "../helpers/token.js";
 import asyncWrapper from "../middleware/asyncWrapper.js";
 import User from "../models/User.js";
-import { registerValidation } from "../validation/authValidation.js";
+import {
+  loginValidation,
+  registerValidation,
+} from "../validation/authValidation.js";
 
 const register = asyncWrapper(async (req, res, next) => {
   /* Validating the request body. */
@@ -27,13 +31,7 @@ const register = asyncWrapper(async (req, res, next) => {
   await user.save();
 
   /* Creating a token for the user. */
-  const token = jwt.sign(
-    { _id: user.id },
-    process.env.ACCESS_TOKEN_SECRET_KEY,
-    {
-      expiresIn: "900s",
-    }
-  );
+  const token =generateToken({_id: user._id}, process.env.ACCESS_TOKEN_SECRET_KEY, "900s");
 
   /* This is the content of the email that will be sent to the user. */
   const subject = "Email confirmation";
@@ -58,6 +56,41 @@ const register = asyncWrapper(async (req, res, next) => {
   });
 });
 
+const login = async (req, res, next) => {
+
+  const result = loginValidation(req.body);
+  if (result.error) return next(newError(result.error.message, 400));
+
+  const user = await User.findOne({ email: result.value.email });
+
+  if (!user) return next(newError("Email doesn't exist", 400));
+
+  if (!user.active)
+    return next(
+      newError("You must verify your email to activate your account", 400)
+    );
+
+  const validPassword = await compareHash(result.value.password, user.password);
+
+  if (!validPassword)
+    return next(newError("Invalid password", 400));
+    const {name, email, role, _id} = user;
+  const accessToken =generateToken({ name, email, role, _id }, process.env.ACCESS_TOKEN_SECRET_KEY, "5m");
+  const refreshToken = generateToken({ name, email, role, _id }, process.env.REFRESH_TOKEN_SECRET_KEY, "1d");
+  
+  user.refreshToken = refreshToken;
+  user.save();
+  res.cookie("jwt", refreshToken, {
+    httpOnly: true,
+    maxAge: 24 * 60 * 60 * 1000,
+  });
+  res.status(200).json({
+    error: false,
+    accessToken: accessToken,
+  });
+};
+
 export default {
   register,
+  login,
 };
